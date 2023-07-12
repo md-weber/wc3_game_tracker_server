c99b828e5dd4a24e7f9e07a6d18c988a47351fbc
Max Weber <weber.max.1988@gmail.com>
2023-07-12 10:07:06 +0200

    [Feature] Created a RouterGroup for API Versioning and introduced a easier way to debug on our HTML page

--- main.go
+++ main.go
@@ -3,28 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"net/http"
-	"time"
+	"wc3_game_tracker/api"
 )
-
-type Tournament struct {
-	Id        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	StartDate time.Time `json:"start_date"`
-	EndDate   time.Time `json:"end_date"`
-}
-
-var tournaments = []Tournament{
-	{Id: uuid.New(), Name: "Creepcamp League", StartDate: time.Now(), EndDate: time.Now().AddDate(0, 0, 3)},
-	{Id: uuid.New(), Name: "League 1", StartDate: time.Now(), EndDate: time.Now().AddDate(0, 0, 4)},
-	{Id: uuid.New(), Name: "League 2", StartDate: time.Now(), EndDate: time.Now().AddDate(0, 0, 5)},
-}
-
-func allTournamentsHandler(context *gin.Context) {
-	// Get all tournaments and return to client
-	context.IndentedJSON(http.StatusOK, tournaments)
-}
 
 func main() {
 	router := gin.Default()
@@ -32,7 +13,8 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", indexHandler)
-	router.GET("/tournaments", allTournamentsHandler)
+
+	api.RegisterTournamentEndpoints(router.Group("/api/v1/"))
 
 	err := router.Run("0.0.0.0:3000")
 
