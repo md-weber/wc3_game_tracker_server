f4c37974229d666299cc05e4938faac54ce5a30c
Max Weber <weber.max.1988@gmail.com>
2023-07-14 12:31:10 +0200

    [Feature] Created a diagram to visiualize the database structure and added the migration to the first DB structure

---
+++ migrations/20230714101029_init.up.sql
@@ -0,0 +1,62 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+CREATE TABLE TOURNAMENT
+(
+    id               uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name             text                                  NOT NULL,
+    type             text                                  NOT NULL,
+    start_date       date,
+    end_date         date,
+    max_participants int
+);
+
+CREATE TABLE PARTICIPANT
+(
+    id           uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name         text                                  NOT NULL,
+    contact_info text,
+    wins         int  DEFAULT 0,
+    losses       int  DEFAULT 0,
+    rank         int  DEFAULT 0
+);
+
+CREATE TABLE TEAM
+(
+    id          uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name        text                                  NOT NULL,
+    members_ids uuid[]                                NOT NULL
+);
+
+CREATE TABLE MATCH
+(
+    id                 uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    participant_one_id uuid,
+    participant_two_id uuid,
+    match_date         timestamp,
+    winner_id          uuid
+);
+
+CREATE TABLE ROUND
+(
+    round_id     uuid,
+    match_ids    uuid[],
+    round_number text
+);
+
+CREATE TABLE BRACKET
+(
+    id        uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    round_ids uuid[]
+);
+
+CREATE TABLE GAME_MAP
+(
+    id   uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text
+);
+
+CREATE TABLE HERO_UNIT
+(
+    id   uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text
+)
